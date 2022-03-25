42fe0ccb50cc6b186606d5afe0abc68d52340399
zjbro <azengjin@gmail.com>
2022-03-25 09:09:27 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/ssfassessment/SsfassessmentApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssfassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfassessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/ssfassessment/SsfassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssfassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
